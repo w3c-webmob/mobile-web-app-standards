e9b447cd16515fdeb950be9dcce014ed4d1ce269
Dominique Hazael-Massieux <dom@w3.org>
2014-01-27 18:43:43 +0100

    only include SVG of editors activity if it exists. close #5

--- js/generate.js
+++ js/generate.js
@@ -219,14 +219,23 @@ function updateEditorsActivity(editorDrafts) {
 	xhr.responseType = "document";
 	xhr.onload = function (draftname) {
 	    return function (e) {
-		var svg = e.target.response;
-		if (svg) {
-		    var height = svg.documentElement.getAttribute("height");
-		    var desc = svg.getElementsByTagNameNS("http://www.w3.org/2000/svg", "desc")[0].textContent;
-		    var draftimages = document.querySelectorAll('object[data="editors-activity/' + draftname + '.svg"');
-		    for (var j = 0 ; j < draftimages.length ; j++) {
+		var svg, height, desc;
+		if (e.target.status == "200" || e.target.status == "304") {
+		    svg = e.target.response;
+		    if (svg) {
+			height = svg.documentElement.getAttribute("height");
+			desc = svg.getElementsByTagNameNS("http://www.w3.org/2000/svg", "desc")[0].textContent;
+		    }
+		}
+		var draftimages = document.querySelectorAll('object[data="editors-activity/' + draftname + '.svg"');
+		for (var j = 0 ; j < draftimages.length ; j++) {
+		    if (svg) {
 			draftimages[j].setAttribute("height",height);
 			draftimages[j].parentNode.querySelector("a").textContent = desc;
+		    } else {
+			draftimages[j].parentNode.querySelector("a").textContent = "@@@ TBD";
+			draftimages[j].remove();
+
 		    }
 		}
 	    };
